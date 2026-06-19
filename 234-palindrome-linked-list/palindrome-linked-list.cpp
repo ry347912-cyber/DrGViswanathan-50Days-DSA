2cdce8f8ca4d73776136791b2dcaa0738e332657
Rupesh Yadav <225709371+ry347912-cyber@users.noreply.github.com>
2026-06-19 12:49:36 +0530

    Time: 0 ms (100.00%) | Memory: 118 MB (64.57%) - LeetSync

---
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        if (!head || !head->next) return true;
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        ListNode* prev = NULL;
+        while (slow) {
+            ListNode* nextNode = slow->next;
+            slow->next = prev;
+            prev = slow;
+            slow = nextNode;
+        }
+        ListNode* left = head;
+        ListNode* right = prev;
+
+        while (right) {
+            if (left->val != right->val)
+                return false;
+            left = left->next;
+            right = right->next;
+        }
+
+        return true;
+    }
+};
