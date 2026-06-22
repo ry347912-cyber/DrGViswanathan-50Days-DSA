c98c67bed441b724f3f3c7b6a41517131510242e
Rupesh Yadav <225709371+ry347912-cyber@users.noreply.github.com>
2026-06-22 11:38:12 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (64.87%) - LeetSync

---
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxNumberOfBalloons(string text) {
+        int counts[26] = {0};
+        for (char c : text) {
+            counts[c - 'a']++;
+        }
+        int b_count = counts['b' - 'a'];
+        int a_count = counts['a' - 'a'];
+        int l_count = counts['l' - 'a'] / 2; 
+        int o_count = counts['o' - 'a'] / 2;
+        int n_count = counts['n' - 'a'];
+        
+     
+        return min({b_count, a_count, l_count, o_count, n_count});
+    }
+};
