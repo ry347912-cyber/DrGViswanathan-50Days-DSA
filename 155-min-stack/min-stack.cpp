3399ae7587162679c3003d71bfb1863d0357cd16
Rupesh Yadav <225709371+ry347912-cyber@users.noreply.github.com>
2026-06-23 16:51:56 +0530

    Time: 48 ms (38.34%) | Memory: 152.3 MB (9.14%) - LeetSync

---
+++ 155-min-stack/min-stack.cpp
@@ -0,0 +1,29 @@
+class MinStack {
+    stack<int> st;
+    stack<int> minSt;
+public:
+    MinStack() {
+    }
+    
+    void push(int val) {
+        st.push(val);
+        if(minSt.empty() || val <= minSt.top()) {
+            minSt.push(val);
+        } else {
+            minSt.push(minSt.top());
+        }
+    }
+    
+    void pop() {
+        st.pop();
+        minSt.pop();
+    }
+    
+    int top() {
+        return st.top();
+    }
+    
+    int getMin() {
+        return minSt.top();
+    }
+};
