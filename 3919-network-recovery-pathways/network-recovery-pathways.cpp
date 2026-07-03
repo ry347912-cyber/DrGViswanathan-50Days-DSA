f53408a342ec44174f4e381f15bef0c615229df9
Rupesh Yadav <225709371+ry347912-cyber@users.noreply.github.com>
2026-07-03 09:18:38 +0530

    Time: 187 ms (79.45%) | Memory: 333.9 MB (65.61%) - LeetSync

---
+++ 3919-network-recovery-pathways/network-recovery-pathways.cpp
@@ -0,0 +1,64 @@
+class Solution {
+public:
+    int findMaxPathScore(vector<vector<int>>& edges, vector<bool>& online, long long k) {
+        int n = online.size();
+
+        vector<vector<pair<int,int>>> g(n);
+        vector<int> indeg(n, 0);
+
+        long long hi = 0;
+        for (auto &e : edges) {
+            g[e[0]].push_back({e[1], e[2]});
+            indeg[e[1]]++;
+            hi = max(hi, (long long)e[2]);
+        }
+
+        // Topological Order
+        vector<int> topo;
+        queue<int> q;
+        for (int i = 0; i < n; i++)
+            if (indeg[i] == 0) q.push(i);
+
+        while (!q.empty()) {
+            int u = q.front();
+            q.pop();
+            topo.push_back(u);
+            for (auto &[v, c] : g[u]) {
+                if (--indeg[v] == 0) q.push(v);
+            }
+        }
+
+        auto check = [&](long long x) -> bool {
+            const long long INF = (1LL << 60);
+            vector<long long> dist(n, INF);
+            dist[0] = 0;
+
+            for (int u : topo) {
+                if (dist[u] == INF) continue;
+                if (u != 0 && u != n - 1 && !online[u]) continue;
+
+                for (auto &[v, w] : g[u]) {
+                    if (w < x) continue;
+                    if (v != n - 1 && v != 0 && !online[v]) continue;
+                    if (dist[v] > dist[u] + w)
+                        dist[v] = dist[u] + w;
+                }
+            }
+
+            return dist[n - 1] <= k;
+        };
+
+        long long lo = 0, ans = -1;
+        while (lo <= hi) {
+            long long mid = (lo + hi) / 2;
+            if (check(mid)) {
+                ans = mid;
+                lo = mid + 1;
+            } else {
+                hi = mid - 1;
+            }
+        }
+
+        return (int)ans;
+    }
+};
