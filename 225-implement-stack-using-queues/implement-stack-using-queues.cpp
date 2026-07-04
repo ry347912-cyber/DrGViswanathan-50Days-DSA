f6a0616d60afa34906237d536fae9d09bbca9f1c
Rupesh Yadav <225709371+ry347912-cyber@users.noreply.github.com>
2026-07-04 11:01:29 +0530

    Time: 1 ms (3.66%) | Memory: 10 MB (39.68%) - LeetSync

---
+++ 225-implement-stack-using-queues/implement-stack-using-queues.cpp
@@ -0,0 +1,31 @@
+class MyStack {
+private:
+    queue<int> q;
+
+public:
+    MyStack() {
+    }
+
+    void push(int x) {
+        q.push(x);
+        int n = q.size();
+        for(int i = 0; i < n-1; i++) {
+            q.push(q.front());
+            q.pop();
+        }
+    }
+
+    int pop() {
+        int val = q.front();
+        q.pop();
+        return val;
+    }
+
+    int top() {
+        return q.front();
+    }
+
+    bool empty() {
+        return q.empty();
+    }
+};
