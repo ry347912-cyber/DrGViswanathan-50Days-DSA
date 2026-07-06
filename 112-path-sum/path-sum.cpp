f61937ef26e1a649373d33979bfcbac7f7bbe71f
Rupesh Yadav <225709371+ry347912-cyber@users.noreply.github.com>
2026-07-06 10:39:11 +0530

    Time: 0 ms (100.00%) | Memory: 21.4 MB (96.22%) - LeetSync

---
+++ 112-path-sum/path-sum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        
+        if(root == NULL)
+           return false;
+        if(root->left == NULL && root->right == NULL){
+             return  targetSum == root->val;
+
+        }
+        targetSum = targetSum -root->val;
+
+        return hasPathSum(root->left, targetSum) ||
+               hasPathSum(root->right, targetSum);
+    }
+};
