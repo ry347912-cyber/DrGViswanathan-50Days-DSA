12d59dbe55bb2db1f10bfd387a743e42c21e0b89
Rupesh Yadav <225709371+ry347912-cyber@users.noreply.github.com>
2026-07-14 07:38:20 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (98.82%) - LeetSync

--- 434-number-of-segments-in-a-string/number-of-segments-in-a-string.cpp
+++ 434-number-of-segments-in-a-string/number-of-segments-in-a-string.cpp
@@ -3,7 +3,7 @@ public:
     int countSegments(string s) {
         int segments = 0;
         for (int i = 0; i < s.length(); i++) {
-            // Check if it's the start of a new segment
+         
             if (s[i] != ' ' && (i == 0 || s[i - 1] == ' ')) {
                 segments++;
             }
